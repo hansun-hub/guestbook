66c3e56b69a2e0a84dbc40e6306ac64f0c5efb77
김한선 <68306239+hansun-hub@users.noreply.github.com>
2023-03-03 10:34:32 +0900

    2023_03-03_Dto를 entity로

--- src/main/java/org/zerock/guestbook/service/GuestbookService.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookService.java
@@ -5,4 +5,14 @@ import org.zerock.guestbook.entity.Guestbook;
 
 public interface GuestbookService {
     Long register(GuestbookDTO dto);
+
+    default Guestbook dtoToEntity(GuestbookDTO dto){
+        Guestbook entity = Guestbook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+        return entity;
+    }
 }
